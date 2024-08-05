da736331828a0e4a9f460ff353cc10734b8d4c18
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-05 15:40:55 +0000

    Code generation

--- components/simple_header_component_1722872375.js
+++ components/simple_header_component_1722872375.js
@@ -4,14 +4,14 @@
 Vue.component("simple_header_component_1722872375", {
     template: `
     <header id="header-section" class="w-full">
-        <nav id="nav-bar" class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
+        <nav id="nav-bar" class="bg-gradient-to-r from-pink-400 to-purple-600 bg-opacity-30 backdrop-filter backdrop-blur-lg shadow-lg rounded-lg py-2.5">
             <div id="nav-container" class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
                 <a id="logo-link" href="#" class="flex items-center">
                     <img id="logo-image" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                    <span id="app-name" class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">EtherScan: Real-Time Ethereum Tracker</span>
+                    <span id="app-name" class="self-center text-xl font-semibold whitespace-nowrap text-white">EtherScan: Real-Time Ethereum Tracker</span>
                 </a>
                 <div id="menu-toggle-button" class="flex items-center lg:order-2">
-                    <button id="collapse-button" data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <button id="collapse-button" data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-white rounded-lg lg:hidden hover:bg-pink-300 hover:bg-opacity-30 focus:outline-none focus:ring-2 focus:ring-pink-300 focus:ring-opacity-50" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <svg id="menu-open-icon" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
@@ -23,12 +23,12 @@ Vue.component("simple_header_component_1722872375", {
                 </div>
                 <div id="mobile-menu-2" class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
                     <ul id="nav-list" class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                        <li id="nav-item-home"><a href="#" class="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white" aria-current="page">Live Transaction Feed</a></li>
-                        <li id="nav-item-1"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Address Monitoring & Alerts</a></li>
-                        <li id="nav-item-2"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Smart Contract Analysis</a></li>
-                        <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Gas Price & Network Analytics</a></li>
+                        <li id="nav-item-home"><a href="#" class="block py-2 pl-3 pr-4 text-white bg-pink-500 bg-opacity-50 rounded-lg lg:bg-transparent lg:text-pink-300 lg:p-0 hover:text-white hover:bg-pink-400 hover:bg-opacity-30 transition-all duration-300" aria-current="page">Live Transaction Feed</a></li>
+                        <li id="nav-item-1"><a href="#" class="block py-2 pl-3 pr-4 text-white hover:bg-pink-400 hover:bg-opacity-30 rounded-lg lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-300 lg:p-0 transition-all duration-300">Address Monitoring & Alerts</a></li>
+                        <li id="nav-item-2"><a href="#" class="block py-2 pl-3 pr-4 text-white hover:bg-pink-400 hover:bg-opacity-30 rounded-lg lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-300 lg:p-0 transition-all duration-300">Smart Contract Analysis</a></li>
+                        <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-white hover:bg-pink-400 hover:bg-opacity-30 rounded-lg lg:hover:bg-transparent lg:border-0 lg:hover:text-pink-300 lg:p-0 transition-all duration-300">Gas Price & Network Analytics</a></li>
                     </ul>
-                    <button class="ml-4 px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50">
+                    <button class="ml-4 px-4 py-2 bg-pink-500 bg-opacity-50 text-white rounded-lg hover:bg-pink-600 hover:bg-opacity-70 focus:outline-none focus:ring-2 focus:ring-pink-300 focus:ring-opacity-50 transition-all duration-300">
                         Hello World
                     </button>
                 </div>
